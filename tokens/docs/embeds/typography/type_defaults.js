8092e5e5e0d2c7ca0843251f0c160ccee00fe264
Matthew Barnett <mattsbarnett@yahoo.com>
2025-09-25 17:00:01 -0400

    test 3

--- tokens/docs/embeds/typography/type_defaults.js
+++ tokens/docs/embeds/typography/type_defaults.js
@@ -1,3 +1,7 @@
+import "../../token_switcher/switcher.js";
+import "../../swatch_maker/swatch_maker.js";
+import "../../typographic_styles/typography.js";
+
 window.tokenSwapDefaults = {
   breakpoint: "mobile"
 };
