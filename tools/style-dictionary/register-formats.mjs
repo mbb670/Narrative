8918980cef226be9d5f6805dd84f50352f527a79
Matthew Barnett <52210222+mbb670@users.noreply.github.com>
2025-09-29 18:16:24 -0400

    Update register-formats.mjs

--- tools/style-dictionary/register-formats.mjs
+++ tools/style-dictionary/register-formats.mjs
@@ -1,9 +1,12 @@
-import StyleDictionary from 'style-dictionary';
-import collectionsFormatter from './formats/format-css-collections.mjs';
+/**
+ * Register custom Style Dictionary formats/filters/transforms
+ * exported by our local format modules.
+ */
+import cssCollections from "./formats/format-css-collections.mjs";
 
-StyleDictionary.registerFormat({
-  name: 'narrative/css-collections',
-  formatter: collectionsFormatter,
-});
-
-export default 'narrative/css-collections';
+export default function registerFormats(StyleDictionary) {
+  StyleDictionary.registerFormat({
+    name: "narrative/css-collections",
+    formatter: cssCollections, // MUST be 'formatter' not 'format'
+  });
+}
