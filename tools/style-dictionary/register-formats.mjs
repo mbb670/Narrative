7f43de9809d35e9116cbd0df9bc9926e6cb5a71a
Matthew Barnett <52210222+mbb670@users.noreply.github.com>
2025-09-29 18:50:04 -0400

    Update register-formats.mjs

--- tools/style-dictionary/register-formats.mjs
+++ tools/style-dictionary/register-formats.mjs
@@ -1,16 +1,5 @@
 // tools/style-dictionary/register-formats.mjs
-import StyleDictionary from "style-dictionary";
-import cssCollections from "./formats/format-css-collections.mjs";
+import StyleDictionary from 'style-dictionary';
+import cssCollections from './formats/format-css-collections.mjs';
 
-export default function registerFormats() {
-  const fmts = [cssCollections];
-
-  fmts.forEach((f) => {
-    if (!f || typeof f.name !== "string" || typeof f.format !== "function") {
-      throw new Error(
-        `Invalid format export. Expected { name: string, format: function }`
-      );
-    }
-    StyleDictionary.registerFormat(f);
-  });
-}
+StyleDictionary.registerFormat(cssCollections);
